78ae06eec8e7093bedc181201161b068f4fe04a7
Larissa Santos <larynha_santos@yahoo.com.br>
2025-12-16 00:09:10 -0300

    ajuste de arquivos

--- backend-ong/src/config/db.js
+++ backend-ong/src/config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "@Renan123",
+    password: process.env.DB_PASSWORD || "ls081189",
     database: process.env.DB_NAME || "bd_ong",
 });
 
